af10a42c22bdc8f2aa717d7a185c47413eaa9d1c
Mariia Savchenko <mashasavchenko@yahoo.com>
2024-07-09 22:31:01 +0300

    fix loadMoreBtn

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -33,6 +33,7 @@ export default function App() {
         setImages([]);
         setPage(1);
         setInputValue(newQuery);
+        setShowBtn(false);
     };
 
     const handleLoadMore = () => {
